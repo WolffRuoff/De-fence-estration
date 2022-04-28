b84ae54e142e237b8da108a10ff40740144008a3
WolffRuoff <ethan@ruoff.us>
2022-04-28 18:00:28 -0400

    Added image segmentation dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,7 @@
 import os
 import random
 import cv2
+from cv2 import inpaint
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -36,12 +37,13 @@ def Create_Mask(fence):
     return mask
 
 class FenceDataset(Dataset):
-    def __init__(self, image_paths, fence_paths, img_transforms=None, fence_transforms=None, combined_transforms=None):
+    def __init__(self, image_paths, fence_paths, img_transforms=None, fence_transforms=None, combined_transforms=None, inpainting=False):
         self.images_paths = image_paths
         self.fence_paths = fence_paths
         self.img_transforms = img_transforms
         self.fence_transforms = fence_transforms
         self.combined_transforms = combined_transforms
+        self.inpainting = inpainting
     
     def __len__(self):
         return len(self.images_paths)
@@ -60,16 +62,28 @@ class FenceDataset(Dataset):
         
         # Convert to tensors
         totensor = transforms.ToTensor()
-        img = totensor(img.convert("RGB"))
+        if self.inpainting:
+            img = totensor(img.convert("RGB"))
+
         combined_img = totensor(combined_img.convert("RGB"))
+        mask = totensor(mask)
         
-        if self.combined_transforms:
+        if self.combined_transforms and self.inpainting:
+            img = totensor(img.convert("RGB"))
             img = self.combined_transforms(img)
             combined_img = self.combined_transforms(combined_img)
+            return combined_img, img, mask
+        elif self.combined_transforms:
+            combined_img = self.combined_transforms(combined_img)
+            return combined_img, mask
+        elif self.inpainting:
+            img = totensor(img.convert("RGB"))
+            return combined_img, img, mask
+        else:
+            return combined_img, mask
         
-        return combined_img, img, mask
 
-def Get_DataLoaders(batch_size, num_workers):
+def Get_DataLoaders(batch_size, num_workers, inpainting=False):
     fence_paths = [f'Fences/{file}' for file in os.listdir('Fences/') if '.png' in file]
     image_paths = Get_Image_Paths()
 
@@ -84,46 +98,74 @@ def Get_DataLoaders(batch_size, num_workers):
     print(f"Image test={len(image_test)}, Fence test={len(fence_test)}")
 
     # Creating the transforms
-    resize_crop = transforms.RandomResizedCrop((224,224), scale=(0.05, 1))
-    resize = transforms.Resize((224,224))
+    resize_crop = transforms.RandomResizedCrop((256,256), scale=(0.05, 1))
+    resize = transforms.Resize((256,256))
     #crop = transforms.RandomCrop((256, 256))
     hflip = transforms.RandomHorizontalFlip(p=0.5)
     jitter = transforms.ColorJitter(brightness=0.3, hue=0.1)
-    blur = transforms.GaussianBlur(9, (0.1,15))
+    blur = transforms.GaussianBlur(7, (0.1,10))
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     img_transforms = transforms.Compose((resize, hflip, jitter))
     fence_transforms = transforms.Compose((resize_crop, hflip, blur))
     #combined_transforms = transforms.Compose((crop, jitter))
+    
+    if inpainting:
+        train_dataset = FenceDataset(image_train, fence_train, img_transforms, fence_transforms, normalize, inpainting)
+        val_dataset = FenceDataset(image_val, fence_val, fence_transforms=fence_transforms, combined_transforms=normalize, inpainting=True)
+        test_dataset = FenceDataset(image_test, fence_test, fence_transforms=fence_transforms, combined_transforms=normalize, inpainting=True)
 
-    train_dataset = FenceDataset(image_train, fence_train, img_transforms, fence_transforms, normalize)
-    val_dataset = FenceDataset(image_val, fence_val, fence_transforms=fence_transforms, combined_transforms=normalize)
-    test_dataset = FenceDataset(image_test, fence_test, fence_transforms=fence_transforms, combined_transforms=normalize)
+        loader_train = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=num_workers, drop_last=True, pin_memory=True)
+        loader_val = DataLoader(val_dataset, batch_size, shuffle=True, num_workers=0, drop_last=True)
+        loader_test = DataLoader(test_dataset, batch_size, shuffle=True, num_workers=0, drop_last=True)
 
-    loader_train = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=num_workers, drop_last=True, pin_memory=True)
-    loader_val = DataLoader(val_dataset, batch_size, shuffle=True, num_workers=0, drop_last=True)
-    loader_test = DataLoader(test_dataset, batch_size, shuffle=True, num_workers=0, drop_last=True)
+        # View a few samples
+        figure = plt.figure(figsize=(30, 10))
+        cols, rows = 5, 3
+        img_batch, label_batch, mask_batch, = next(iter(loader_train))
+        for i in range(1, cols+1):
+            input, label, mask = img_batch[i], label_batch[i], mask_batch[i]
+            figure.add_subplot(rows, cols, i)
+            plt.title(f"Image {i}")
+            plt.axis("off")
+            plt.imshow(unnormalize(input).permute(1, 2, 0))
+            j = cols + i
+            figure.add_subplot(rows, cols, j)
+            plt.title(f"Label {i}")
+            plt.axis("off")
+            plt.imshow(unnormalize(label).permute(1, 2, 0))
+            k = (cols * 2) + i
+            figure.add_subplot(rows, cols, k)
+            plt.title(f"mask {i}")
+            plt.axis("off")
+            plt.imshow(mask)
+        plt.show()
 
-    # View a few samples
-    figure = plt.figure(figsize=(30, 10))
-    cols, rows = 5, 3
-    img_batch, label_batch, mask_batch, = next(iter(loader_train))
-    for i in range(1, cols+1):
-        input, label, mask = img_batch[i], label_batch[i], mask_batch[i]
-        figure.add_subplot(rows, cols, i)
-        plt.title(f"Image {i}")
-        plt.axis("off")
-        plt.imshow(unnormalize(input).permute(1, 2, 0))
-        j = cols + i
-        figure.add_subplot(rows, cols, j)
-        plt.title(f"Label {i}")
-        plt.axis("off")
-        plt.imshow(unnormalize(label).permute(1, 2, 0))
-        k = (cols * 2) + i
-        figure.add_subplot(rows, cols, k)
-        plt.title(f"mask {i}")
-        plt.axis("off")
-        plt.imshow(mask)
-    plt.show()
+        return loader_train, loader_val, loader_test
+    else:
+        train_dataset = FenceDataset(image_train, fence_train, img_transforms, fence_transforms, normalize)
+        val_dataset = FenceDataset(image_val, fence_val, fence_transforms=fence_transforms, combined_transforms=normalize)
+        test_dataset = FenceDataset(image_test, fence_test, fence_transforms=fence_transforms, combined_transforms=normalize)
 
-    return loader_train, loader_val, loader_test
+        loader_train = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=num_workers, drop_last=True, pin_memory=True)
+        loader_val = DataLoader(val_dataset, batch_size, shuffle=True, num_workers=0, drop_last=True)
+        loader_test = DataLoader(test_dataset, batch_size, shuffle=True, num_workers=0, drop_last=True)
+
+        # View a few samples
+        figure = plt.figure(figsize=(30, 10))
+        cols, rows = 5, 2
+        img_batch, mask_batch, = next(iter(loader_train))
+        for i in range(1, cols+1):
+            input, mask = img_batch[i], mask_batch[i]
+            figure.add_subplot(rows, cols, i)
+            plt.title(f"Image {i}")
+            plt.axis("off")
+            plt.imshow(unnormalize(input).permute(1, 2, 0))
+            j = cols + i
+            figure.add_subplot(rows, cols, j)
+            plt.title(f"Mask {i}")
+            plt.axis("off")
+            plt.imshow(mask.permute(1, 2, 0))
+        plt.show()
+
+        return loader_train, loader_val, loader_test
